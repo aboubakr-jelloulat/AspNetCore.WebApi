f8278a299c2505827ead3c293199bed7ffa75e9b
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-01-06 17:30:54 +0100

    Remove default setup & add data base script

---
+++ DURC/script.sh
@@ -0,0 +1,20 @@
+
+--create database sparka;
+
+--go
+
+--use sparka;
+
+--go
+
+--create table Products
+--	(
+--		Id		int primary key identity,
+--		Name	nvarchar(50) NOT NULL,
+--		sku		nvarchar(50) NOT NULL
+--	);
+
+
+
+use sparka;
+SELECT * From  Products
